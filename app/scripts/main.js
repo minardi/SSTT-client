2c37b6e007d3e744e3d4f1cfd34359b6b96d888c
Yuriy <culer2@yandex.ru>
2014-02-24 21:04:05 +0200

    Initial status

---
+++ app/scripts/main.js
@@ -0,0 +1,11 @@
+
+
+var app = {
+                  ScrumPage: {},
+                  empty: {}
+               };
+
+$(function () {
+    'use strict';    
+	var scrumView = new app.ScrumPage.ModelView({el: $('#in')});
+});
