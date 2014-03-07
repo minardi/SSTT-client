04cb246fce2b4ae0bd80d01c53b9bb38e3f2e74e
bigunaleksej <bigunaleksej@gmail.com>
2014-03-07 17:47:38 +0200

    change dash

--- app/scripts/DashBoard/DashBoardCollection.js
+++ app/scripts/DashBoard/DashBoardCollection.js
@@ -4,6 +4,70 @@
 
     module.Collection = Backbone.Collection.extend({
 
+    	initialize: function() {
+    		/*
+			this.add(<%= %>)
+    		*/
+
+    		this.add([
+                {
+                    content: "Back",
+                    glyph: "glyphicon glyphicon-arrow-left",
+                    permition: {
+                        "must_be": "",
+                        "not": {
+                            page: ["project_page", "team_edit_page"]
+                        }
+                    }
+                },
+
+                {
+                    content: "BackFromTeamEditPage", 
+                    glyph: "glyphicon glyphicon-arrow-left",
+                    permition: {
+                        "must_be": {
+                            page: ["team_edit_page"]
+                        },
+                        "not": ""
+                    }
+                },
+
+                {
+                    content: "Team",
+                    permition: {
+                        "must_be": {
+                            right: ["pm"]
+                        },
+                        "not": {
+                            page: ["team_page", "team_edit_page"]
+                        }
+                    }
+                },
+
+                {
+                    content: "Configure",
+                    glyph: "glyphicon glyphicon-arrow-cog",
+                    permition: {
+                        "must_be": {
+                            right: ["pm"]
+                        },
+                        "not": ""
+                    }
+                },
+
+                {
+                    content: "Delete",
+                    glyph: "glyphicon glyphicon-arrow-remove",
+                    permition: {
+                        "must_be": {
+                            right: ["pm"]
+                        },
+                        "not": ""
+                    }
+                } 
+            ])
+		},
+
         model: module.Model,
 
     });
