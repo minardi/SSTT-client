34bf6e59727c8bc7cff9663066ff8880a4827940
Yuriy <culer2@yandex.ru>
2014-03-16 20:20:39 +0200

    Save fix

--- app/scripts/Routing/Router.js
+++ app/scripts/Routing/Router.js
@@ -5,11 +5,15 @@
     module.Router = Backbone.Router.extend({
 
         routes: {
+            "teams/:id/team_edit_page": "showTeamEditPage",
             "" : "index"
         },
 
         index: function() {
             console.log("I am Router");
+        },
+        
+        showTeamEditPage: function() {
         }
 
     });
