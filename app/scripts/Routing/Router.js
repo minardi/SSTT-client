266187cace56cb4c2224eb93166b3d9dfdcacfa0
OlegKrutas <krutas_1990@mail.ru>
2014-03-18 15:26:03 +0200

    Saving

--- app/scripts/Routing/Router.js
+++ app/scripts/Routing/Router.js
@@ -3,7 +3,7 @@
 (function(module) {
 
     module.Router = Backbone.Router.extend({
-
+       
         initialize: function() {
             mediator.sub("ProjectPage:ProjectChecked", this.toProject, this);
             mediator.sub("ProjectPage:ProjectSelected", this.toScrumPage, this);
@@ -19,25 +19,35 @@
             "project_url": "",
             "scrum_url": "",
             "team_url": ""
-        },
+        }, 
 
-        routes: {
-            "": "",
-            "" :  "index",
+         routes: {            
+            "" :  "index",            
+            "project-:id" : "projectInfo",
+            "project-:id/scrum-page" : "scrumPage"
         },
 
         index: function() {
             console.log("I am Router");           
         },
 
+        projectInfo: function(id) {
+            console.log('project');
+            //mediator.pub("ProjectPage:ProjectChecked", id);
+        },
+
+        scrumPage: function(id) {
+            console.log('scrum' + id);
+        }, 
+          
         toProject: function(model) {
             this.navigate("project-" + model.get("id"));
         },
 
         toScrumPage: function(project_id) {
             this.urls["scrum_url"] = "project-" + project_id + "/scrum-page";
-            
-            this.navigate("project-" + project_id + "/scrum-page");
+
+            this.navigate("project-" + project_id + "/scrum-page", {trigger: true});
         },
 
         toPlanning: function() {
@@ -67,7 +77,7 @@
 
         toTeamEditPageTab: function(tab_selected) {
             this.navigate(this.urls["team_url"] + "/" + tab_selected);
-        }
+        }     
 
     });
 
